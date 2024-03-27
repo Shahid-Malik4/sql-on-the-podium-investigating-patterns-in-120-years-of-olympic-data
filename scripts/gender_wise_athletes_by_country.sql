bca5b1d805ec19bf9fbd32d90e7eff18ed6e169d
Shahid Malik <79898163+shahidmalik4@users.noreply.github.com>
2024-03-27 11:39:33 +0500

    Update gender_wise_athletes_by_country.sql

--- scripts/gender_wise_athletes_by_country.sql
+++ scripts/gender_wise_athletes_by_country.sql
@@ -8,3 +8,19 @@ INNER JOIN regions r ON e.noc = r.noc
 GROUP BY country
 ORDER BY total_athletes DESC
 LIMIT 10;
+
+/*
+| Country   | Total Athletes | Male  | Female |
+|-----------|----------------|-------|--------|
+| USA       | 18034          | 12600 | 5434   |
+| Germany   | 15681          | 10860 | 4821   |
+| Russia    | 11501          | 7404  | 4097   |
+| UK        | 11488          | 8258  | 3230   |
+| France    | 11404          | 8706  | 2698   |
+| Italy     | 10321          | 7796  | 2525   |
+| Canada    | 9501           | 5968  | 3533   |
+| Japan     | 8327           | 5556  | 2771   |
+| Sweden    | 8327           | 6343  | 1984   |
+| Australia | 7599           | 4869  | 2730   |
+
+*/
