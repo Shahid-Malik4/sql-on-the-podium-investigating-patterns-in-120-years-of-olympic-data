19fc2612d9b473bc892b57c4f47be1e14b31c041
Shahid Malik <79898163+shahidmalik4@users.noreply.github.com>
2024-03-27 11:48:37 +0500

    Update regions_medals_distribution_by_season.sql

--- scripts/regions_medals_distribution_by_season.sql
+++ scripts/regions_medals_distribution_by_season.sql
@@ -6,3 +6,33 @@ SELECT
 FROM events e
 INNER JOIN regions r ON e.noc = r.noc 
 GROUP BY region;
+
+/*
+| Region                    | Summer_Medals | Winter_Medals | Total_Medals |
+|---------------------------|---------------|---------------|--------------|
+| China                     | 913           | 80            | 993          |
+| Denmark                   | 591           | 5             | 596          |
+| Netherlands               | 906           | 122           | 1028         |
+| USA                       | 4790          | 602           | 5392         |
+| Finland                   | 474           | 426           | 900          |
+| Norway                    | 590           | 443           | 1033         |
+| Romania                   | 634           | 2             | 636          |
+| Estonia                   | 43            | 7             | 50           |
+| France                    | 1531          | 150           | 1681         |
+| Morocco                   | 23            | 0             | 23           |
+| Spain                     | 487           | 2             | 489          |
+| Egypt                     | 25            | 0             | 25           |
+| Bulgaria                  | 336           | 6             | 342          |
+| Italy                     | 1407          | 191           | 1598         |
+| Azerbaijan                | 44            | 0             | 44           |
+| Russia                    | 3185          | 757           | 3942         |
+| Argentina                 | 274           | 0             | 274          |
+| Cuba                      | 409           | 0             | 409          |
+| Belarus                   | 124           | 15            | 139          |
+| Greece                    | 121           | 0             | 121          |
+| Cameroon                  | 22            | 0             | 22           |
+| Turkey                    | 79            | 0             | 79           |
+| Chile                     | 32            | 0             | 32           |
+| Mexico                    | 110           | 0             | 110          |
+
+*/
