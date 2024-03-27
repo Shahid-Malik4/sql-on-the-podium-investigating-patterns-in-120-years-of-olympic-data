833654b8005a3f7a5d8b92407473567c3194c30b
Shahid Malik <79898163+shahidmalik4@users.noreply.github.com>
2024-03-27 11:49:15 +0500

    Update regions_participated_in_every_olympic.sql

--- scripts/regions_participated_in_every_olympic.sql
+++ scripts/regions_participated_in_every_olympic.sql
@@ -4,3 +4,14 @@ FROM regions r
 INNER JOIN events e ON r.noc = e.noc
 GROUP BY region
 HAVING COUNT(DISTINCT e.year) = (SELECT COUNT(DISTINCT year) FROM events);
+
+/*
+| Region       |
+|--------------|
+| Australia    |
+| Greece       |
+| Switzerland  |
+| UK           |
+| USA          |
+
+*/
