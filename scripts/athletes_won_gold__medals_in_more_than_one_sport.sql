3b733d36468ae6db9baab94d97171c6b7ef7f4f2
Shahid Malik <79898163+shahidmalik4@users.noreply.github.com>
2024-03-27 11:37:17 +0500

    Update athletes_won_gold__medals_in_more_than_one_sport.sql

--- scripts/athletes_won_gold__medals_in_more_than_one_sport.sql
+++ scripts/athletes_won_gold__medals_in_more_than_one_sport.sql
@@ -7,3 +7,14 @@ GROUP BY athlete
 HAVING COUNT(DISTINCT sport) > 1
 ORDER BY sports DESC
 LIMIT 5;
+
+/*
+| Athlete                             | Sports |
+|-------------------------------------|--------|
+| Aleksandr Vladimirovich Popov       | 2      |
+| Anfisa Anatolyevna Reztsova         | 2      |
+| Carl Frederik Pedersen              | 2      |
+| Carl Schuhmann                      | 2      |
+| Charles Frederick "Karch" Kiraly    | 2      |
+
+*/
