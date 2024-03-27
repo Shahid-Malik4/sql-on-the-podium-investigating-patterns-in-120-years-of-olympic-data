537292eb6cf73e96118cc1b618daf05b74ca21ef
Shahid Malik <79898163+shahidmalik4@users.noreply.github.com>
2024-03-27 11:37:55 +0500

    Update distributions_of_years_by_regions_and_athletes_participated.sql

--- scripts/distributions_of_years_by_regions_and_athletes_participated.sql
+++ scripts/distributions_of_years_by_regions_and_athletes_participated.sql
@@ -7,3 +7,14 @@ FROM events
 GROUP BY year
 ORDER BY regions_participated DESC
 LIMIT 5;
+
+/*
+| Year | Regions Participated | Athletes Participated | Medals |
+|------|----------------------|-----------------------|--------|
+| 2016 | 207                  | 11176                 | 2022   |
+| 2012 | 205                  | 10512                 | 1940   |
+| 2008 | 204                  | 10895                 | 2048   |
+| 2004 | 201                  | 10554                 | 2000   |
+| 2000 | 200                  | 10641                 | 2003   |
+
+*/
