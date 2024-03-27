9a63a0808049b328cbd11740597b22f13915cbf4
Shahid Malik <79898163+shahidmalik4@users.noreply.github.com>
2024-03-27 11:50:56 +0500

    Update regions_won_gold_medals_in_every_olympic.sql

--- scripts/regions_won_gold_medals_in_every_olympic.sql
+++ scripts/regions_won_gold_medals_in_every_olympic.sql
@@ -5,3 +5,10 @@ INNER JOIN events e ON r.noc = e.noc
 WHERE e.medal = 'Gold'
 GROUP BY region
 HAVING COUNT(DISTINCT e.year) = (SELECT COUNT(DISTINCT year) FROM events);
+
+/*
+| Region |
+|--------|
+| USA    |
+
+*/
